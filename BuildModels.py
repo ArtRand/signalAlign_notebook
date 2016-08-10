c7e8e576e0e02d8b8f411d4512215d48348e12e5
ArtRand <arand@soe.ucsc.edu>
2016-08-09 18:43:02 -0700

    ready for large scale testing

--- BuildModels.py
+++ BuildModels.py
@@ -5,7 +5,8 @@ import sys
 import glob
 import pandas as pd
 import numpy as np
-from subprocess import Popen, check_call
+from argparse import ArgumentParser
+from subprocess import Popen
 from itertools import product
 from commonFunctions import get_first_seq, make_motif_file, get_all_sequence_kmers
 
@@ -30,6 +31,7 @@ def find_gatc_motifs(sequence):
 
 
 def make_gatc_position_file(fasta, outfile):
+    outfile = os.path.abspath(outfile)
     fH = open(outfile, 'w')
     fH.write("X\t")
 
@@ -45,17 +47,19 @@ def make_gatc_position_file(fasta, outfile):
         fH.write("{}\t".format(t_pos))  # + 1 because this is for the reverse complement
     fH.write("\n")
     fH.close()
-    return
+    return outfile
 
 
 def make_gatc_motif_file(fasta, outfile):
+    outfile = os.path.abspath(outfile)
     seq = get_first_seq(fasta)
     gatcs = [x for x in find_gatc_motifs(seq)]
     make_motif_file(gatcs, seq, outfile)
+    return outfile
 
 
-def run_guide_alignment(fasta, pcr_reads, genomic_reads, jobs, positions_file, motif_file, n, t_model, c_model,
-                        outpath):
+def run_guide_alignment(fasta, pcr_reads, genomic_reads, jobs, positions_file, motif_file, t_model, c_model,
+                        outpath, n):
     working_path = os.path.abspath(outpath)
     commands = []
     c = PATH_TO_BINS + "runSignalAlign -d={reads} -r={fasta} -T={tModel} -C={cModel} -f=assignments " \
@@ -65,8 +69,8 @@ def run_guide_alignment(fasta, pcr_reads, genomic_reads, jobs, positions_file, m
     labels = ["A", "I"]
     working_directories = [working_path + "/pcr_", working_path + "/genomic_"]
 
-    assert os.path.exists(t_model), "Didn't find template model"
-    assert os.path.exists(c_model), "Didn't find complement model"
+    assert os.path.exists(t_model), "Didn't find template model, looked {}".format(t_model)
+    assert os.path.exists(c_model), "Didn't find complement model, looked {}".format(c_model)
 
     for reads, label, working_directory in zip(read_sets, labels, working_directories):
         # assemble the command
@@ -104,22 +108,24 @@ def make_master_assignment_table(assignment_directories):
 def train_model_transitions(fasta, pcr_reads, genomic_reads, jobs, positions_file, iterations, batch_size, outpath,
                             stateMachine="threeState", t_hdp=None, c_hdp=None):
     working_path = os.path.abspath(outpath) + "/"
-
+    model_directory = working_path + "{}_".format(stateMachine)
     t_model = PATH_TO_SIGNALALIGN + "/models/testModelR9_5mer_acegit_template.model"
     c_model = PATH_TO_SIGNALALIGN + "/models/testModelR9_5mer_acegit_complement.model"
+    assert os.path.exists(t_model), "Didn't find template model"
+    assert os.path.exists(c_model), "Didn't find complement model"
 
     os.chdir(PATH_TO_BINS)
     c = "trainModels -d={pcr} -d={genomic} -X=C -X=I -r={fasta} -i={iter} -a={batch} --transitions -smt={smt} " \
         "-T={tModel} -C={cModel} -j={jobs} -x=adenosine -p={positions} -o={out} " \
         "".format(pcr=pcr_reads, genomic=genomic_reads, fasta=fasta, iter=iterations, batch=batch_size,
                   smt=stateMachine, tModel=t_model, cModel=c_model, jobs=jobs,
-                  positions=os.path.abspath(positions_file), out=working_path)
+                  positions=positions_file, out=model_directory)
     if t_hdp is not None and c_hdp is not None:
         c += "-tH={tHdp} -cH={cHdp} ".format(tHdp=os.path.abspath(t_hdp), cHdp=os.path.abspath(c_hdp))
     c = PATH_TO_BINS + c
     os.system(c)
-    models = [working_path + "tempFiles_expectations/template_trained.hmm",
-              working_path + "tempFiles_expectations/complement_trained.hmm"]
+    models = [model_directory + "tempFiles_expectations/template_trained.hmm",
+              model_directory + "tempFiles_expectations/complement_trained.hmm"]
     os.chdir(working_path)
     return models
 
@@ -142,7 +148,7 @@ def make_build_alignment(assignments, ref_fasta, num_assignments, outfile, thres
                 if n >= num_assignments:
                     break
     fH.close()
-    return
+    return outfile
 
 
 def build_hdp(build_alignment_path, template_model, complement_model, outpath, samples=15000):
@@ -159,31 +165,84 @@ def build_hdp(build_alignment_path, template_model, complement_model, outpath, s
                                   out=hdp_pipeline_dir)
     c = PATH_TO_BINS + c
     os.system(c)
-    return
+    os.chdir(outpath)
+    return [hdp_pipeline_dir + "template.multisetPriorEcoli.nhdp",
+            hdp_pipeline_dir + "complement.multisetPriorEcoli.nhdp"]
 
 
 def main(args):
+    def parse_args():
+        parser = ArgumentParser(description=__doc__)
+        parser.add_argument("-r", action="store", dest="reference", required=True)
+        parser.add_argument("-pcr", action="store", dest="pcr_reads", required=True)
+        parser.add_argument("-gen", action="store", dest="genomic_reads", required=True)
+        parser.add_argument("-o", action="store", dest="outpath", required=True)
+        parser.add_argument("-j", action="store", dest="jobs", required=False, default=4, type=int)
+        parser.add_argument("-i", action="store", dest="iterations", required=False, type=int, default=20)
+        parser.add_argument("-a", action="store", dest="batch", required=False, type=int, default=15000)
+        parser.add_argument("-s", action="store", dest="assignments", required=False, type=int, default=100)
+        parser.add_argument("-n", action="store", dest="n_aligns", required=False, type=int, default=400)
+        parser.add_argument("-t", action="store", dest="assignment_threshold", required=False, type=float, default=0.3)
+        parser.add_argument("-g", action="store", dest="samples", required=False, type=int, default=15000)
+        args = parser.parse_args()
+        return args
+    args = parse_args()
+
+    # make the positions and motif file
+    working_path = os.path.abspath(args.outpath)
+    positions_file = make_gatc_position_file(args.reference, working_path + "/gatc_positions.positions")
+
+    # make the motif file
+    motif_file = make_gatc_motif_file(args.reference, working_path + "/gatc_target.target")
+
     # train the transitions
-    models = train_model_transitions("/Users/Rand/projects/marginAlign/notebooks/pUC19_SspI.fa",
-                                     "/Users/Rand/projects/marginAlign/sandbox/R9_pUC/NB07_pcr/",
-                                     "/Users/Rand/projects/marginAlign/sandbox/R9_pUC/NB08_genomic/", 4,
-                                     "/Users/Rand/projects/marginAlign/notebooks/pUC_gatc.positions",
-                                     1, 2000, "./")
+    models = train_model_transitions(fasta=os.path.abspath(args.reference),
+                                     pcr_reads=os.path.abspath(args.pcr_reads) + "/",
+                                     genomic_reads=os.path.abspath(args.genomic_reads) + "/",
+                                     jobs=args.jobs,
+                                     positions_file=positions_file,
+                                     iterations=args.iterations,
+                                     batch_size=args.batch,
+                                     outpath=working_path,
+                                     )
+
     # do the initial alignments
-    assignment_dirs = run_guide_alignment(fasta="/Users/Rand/projects/marginAlign/notebooks/pUC19_SspI.fa",
-                                          pcr_reads="/Users/Rand/projects/marginAlign/sandbox/R9_pUC/NB07_pcr/",
-                                          genomic_reads="/Users/Rand/projects/marginAlign/sandbox/R9_pUC/NB08_genomic/",
-                                          jobs=4,
-                                          positions_file="/Users/Rand/projects/marginAlign/notebooks/pUC_gatc.positions",
-                                          motif_file="/Users/Rand/projects/marginAlign/notebooks/pUC_gatc.target",
-                                          n=2,
+    assignment_dirs = run_guide_alignment(fasta=os.path.abspath(args.reference),
+                                          pcr_reads=os.path.abspath(args.pcr_reads) + "/",
+                                          genomic_reads=os.path.abspath(args.genomic_reads) + "/",
+                                          jobs=args.jobs,
+                                          positions_file=positions_file,
+                                          motif_file=motif_file,
+                                          n=args.n_aligns,
                                           t_model=models[0],
                                           c_model=models[1],
-                                          outpath="/Users/Rand/projects/marginAlign/notebooks/")
-
+                                          outpath=working_path)
+    # concatenate the assignments into table
     master = make_master_assignment_table(assignment_dirs)
-    make_build_alignment(master, "/Users/Rand/projects/marginAlign/notebooks/pUC19_SspI.fa", 3, "./buildAlignment.tsv")
-    build_hdp("./buildAlignment.tsv", models[0], models[1], "./", 200)
+    build_alignment = make_build_alignment(assignments=master,
+                                           ref_fasta=os.path.abspath(args.reference),
+                                           num_assignments=args.assignments,
+                                           outfile=working_path + "/buildAlignment.tsv",
+                                           threshold=args.assignment_threshold)
+    # build hdp
+    hdps = build_hdp(build_alignment_path=build_alignment,
+                     template_model=models[0],
+                     complement_model=models[1],
+                     outpath=working_path,
+                     samples=args.samples)
+    # train HMM/HDP
+    models = train_model_transitions(fasta=os.path.abspath(args.reference),
+                                     pcr_reads=os.path.abspath(args.pcr_reads) + "/",
+                                     genomic_reads=os.path.abspath(args.genomic_reads) + "/",
+                                     jobs=args.jobs,
+                                     positions_file=positions_file,
+                                     iterations=args.iterations,
+                                     batch_size=args.batch,
+                                     outpath=working_path,
+                                     stateMachine="threeStateHdp",
+                                     t_hdp=hdps[0],
+                                     c_hdp=hdps[1],
+                                     )
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
